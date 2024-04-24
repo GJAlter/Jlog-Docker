f9acc108f35fcb97d39c36488109b5aeb6b96fa9
Jun <gjdeveloper@vers.kr>
2024-04-24 15:58:55 +0900

    echo 추가

--- commands.sh
+++ commands.sh
@@ -20,3 +20,5 @@ docker run -d -p 8080:8080 -e DB_HOST=jlog-db -e DB_ID=gjdeveloper -e DB_PW=1234
 echo "=> Frontend(React(ts), Nginx) Build & Run"
 docker build -t gjdeveloper/jlog-front:1.1.0 .
 docker run -d -p 3000:3000 -v jlog-volume-resource:/usr/local/jlog/files --network jlog-network --name jlog-front gjdeveloper/jlog-front:1.1.0
+
+echo "...Finish Docker Deploy"s
