f6b27ae779320140ea924132e11bf55d7b2cdcd9
Jun <gjdeveloper@vers.kr>
2024-04-25 00:05:49 +0900

    Tomcat 파일 업로드 제한 해제

--- commands.sh
+++ commands.sh
@@ -13,12 +13,12 @@ docker run -d -p 27017:27017 -v jlog-volume-db:/data/db --network jlog-network -
 
 # backend
 echo "=> Backend(Spring Boot, Tomcat) Build & Run"
-docker build -t gjdeveloper/jlog-back:1.1.0 .
-docker run -d -p 8080:8080 -e DB_HOST=jlog-db -e DB_ID=gjdeveloper -e DB_PW=1234qwer! -v jlog-volume-resource:/usr/local/jlog/files --network jlog-network --name jlog-back gjdeveloper/jlog-back:1.1.0
+docker build -t gjdeveloper/jlog-back:1.1.1 .
+docker run -d -p 8080:8080 -e DB_HOST=jlog-db -e DB_ID=gjdeveloper -e DB_PW=1234qwer! -v jlog-volume-resource:/usr/local/jlog/files --network jlog-network --name jlog-back gjdeveloper/jlog-back:1.1.1
 
 # frontend
 echo "=> Frontend(React(ts), Nginx) Build & Run"
-docker build -t gjdeveloper/jlog-front:1.1.0 .
-docker run -d -p 3000:3000 -v jlog-volume-resource:/usr/local/jlog/files --network jlog-network --name jlog-front gjdeveloper/jlog-front:1.1.0
+docker build -t gjdeveloper/jlog-front:1.1.1 .
+docker run -d -p 3000:3000 -v jlog-volume-resource:/usr/local/jlog/files --network jlog-network --name jlog-front gjdeveloper/jlog-front:1.1.1
 
 echo "...Finish Docker Deploy"s
